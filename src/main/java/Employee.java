9b56d86bf2fad4157fc9e76143645cda1065f0fa
Сергей Покровский <pokrovsky.s.n@gmail.com>
2022-05-11 18:10:00 +0300

    third commit

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -17,14 +17,33 @@ public class Employee {
         this.age = age;
     }
 
-//    @Override
-//    public String toString(){
-//        return   "{ \n" +
-//                "id: " + id +
-//                ", \n firstName: " + firstName +
-//                ", \n lastName: " + lastName +
-//                ", \n country: " + country +
-//                ", \n age: " + age +
-//                "\n }";
-//    }
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+        @Override
+    public String toString(){
+        return   "{ \n" +
+                "id: " + id +
+                ", \n firstName: " + firstName +
+                ", \n lastName: " + lastName +
+                ", \n country: " + country +
+                ", \n age: " + age +
+                "\n }";
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,15 +6,19 @@ import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.jar.Attributes;
 
 public class Main {
 
@@ -35,55 +39,89 @@ public class Main {
         return list;
     }
 
-    public static String listToJson(List<Employee> list){
+    public static String listToJson(List<Employee> list) {
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
-        Type listType = new TypeToken<List<Employee>>() {}.getType();
+        Type listType = new TypeToken<List<Employee>>() {
+        }.getType();
         String json = gson.toJson(list, listType);
         return json;
     }
 
-    public static void writeString(String json, String fileName){
-        try (FileWriter  fileWriter = new FileWriter(fileName)){
+    public static void writeString(String json, String fileName) {
+        try (FileWriter fileWriter = new FileWriter(fileName)) {
             fileWriter.write(json);
             fileWriter.flush();
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    public static List<Employee> parseXML (String fileName){
+
+    public static List<Employee> parseXML(File file) {
         List<Employee> list = new ArrayList<>();
         try {
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = factory.newDocumentBuilder();
-            Document document = builder.newDocument();
-            Node root = document.getDocumentElement();
-            NodeList nodeList = root.getChildNodes();
-            for (int i =0; i < nodeList.getLength(); i++){
+            Document document = builder.parse("data.xml");
+
+            Node staffNode = document.getFirstChild();
+            NodeList nodeList = staffNode.getChildNodes();
+            long id = 0;
+            String firstName = "";
+            String lastName = "";
+            String country = "";
+            int age = 0;
+            for (int i = 0; i < nodeList.getLength(); i++) {
                 Node node = nodeList.item(i);
-                if (Node.ELEMENT_NODE == node.getNodeType()){
-                    Element element = (Element) node;
-                    NamedNodeMap map = element.getAttributes();
-                    for (int a = 0; a < map.getLength(); a++){
-                        String AttrName = map.item(a).getNodeName();
-                        String AttrValue = map.item(a).getNodeValue();
-                        Employee employee =
-                    }
-
+                if (node.getNodeType() != Node.ELEMENT_NODE) {
+                    continue;
                 }
+                NodeList employeeChilds = node.getChildNodes();
+                for (int j = 0; j < employeeChilds.getLength(); j++) {
+                    Node atr = employeeChilds.item(j);
+                    if (employeeChilds.item(j).getNodeType() != Node.ELEMENT_NODE) {
+                        continue;
+                    }
+                    switch (atr.getNodeName()) {
+                        case "id": {
+                            id = Long.valueOf(atr.getTextContent());
+                            break;
+                        }
+                        case "firstName": {
+                            firstName = atr.getTextContent();
+                            break;
+                        }
+                        case "lastName": {
+                            lastName = atr.getTextContent();
+                            break;
+                        }
+                        case "country": {
+                            country = atr.getTextContent();
+                            break;
+                        }
+                        case "age": {
+                            age = Integer.valueOf(atr.getTextContent());
+                            break;
+                        }
+                    }
+                }
+                Employee employee = new Employee(id, firstName, lastName, country, age);
+                list.add(employee);
             }
-
-        } catch (ParserConfigurationException ex) {
+        } catch (ParserConfigurationException | IOException | SAXException ex) {
             ex.printStackTrace();
         }
         return list;
     }
+
     public static void main(String[] args) {
         String fileName = "data.csv";
         List<Employee> list = parseCSV(fileName);
         String json = listToJson(list);
         writeString(json, "data.json");
-        List<Employee> list1 = parseXML("data.xml");
+
+        File file = new File("data.xml");
+        List<Employee> list1 = parseXML(file);
         String json1 = listToJson(list1);
         writeString(json1, "data1.json");
     }
